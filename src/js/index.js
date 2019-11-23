89663b8e3b4a03e6d211f4a70bc74fea263bdb29
malekz <malek.zarzour@gmail.com>
2019-11-23 23:52:25 +0100

    rendered the recipe into UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,35 @@
-// Global app controller
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
 
-import num from "./test";
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - liked recipe
+ */
+const state = {};
 
-console.log(`I imported ${num} from another module`);
+const controlSearch = async () => {
+  // 1) Get query from view
+  const query = searchView.getInput();
+
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
+
+    // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResult();
+    // 4) Search for recipe
+    await state.search.getResults();
+
+    // 5) Render results on UI
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
